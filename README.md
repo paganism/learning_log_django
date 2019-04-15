bcfad7bdc6842949e37d2d25f66a0b7eaabcf1db
YuriyB <paganism@mail.ru>
2019-04-15 23:07:58 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# learning_log_django
+
+Learning Log helps you keep track of your learnong foe any topic you are learnong about.
---
+++ learning_logs/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('topics/', views.topics, name='topics'),
+    path('topics/<pk>', views.topic, name='topic')
+]
