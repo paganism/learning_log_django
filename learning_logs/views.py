19428285aac771bb6c4a7ea14ae1120ad79c17a7
YuriyB <paganism@mail.ru>
2019-04-21 20:45:48 +0300

    added login_required

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -3,18 +3,21 @@ from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 from django.http import HttpResponseRedirect
 from django.shortcuts import reverse
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
     return render(request, 'learning_logs/index.html')
 
 
+@login_required
 def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
 
+@login_required
 def topic(request, pk):
     topic = Topic.objects.get(id=pk)
     entries = topic.entry_set.order_by('-date_added')
@@ -22,6 +25,7 @@ def topic(request, pk):
     return render(request, 'learning_logs/topic.html', context)
 
 
+@login_required
 def new_topic(request):
     if request.method != 'POST':
         form = TopicForm()
@@ -33,6 +37,7 @@ def new_topic(request):
     return render(request, 'learning_logs/new_topic.html', context)
 
 
+@login_required
 def new_entry(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     if request.method != 'POST':
@@ -49,6 +54,7 @@ def new_entry(request, topic_id):
     return render(request, 'learning_logs/new_entry.html', context)
 
 
+@login_required
 def edit_entry(request, entry_id):
 
     entry = Entry.objects.get(id=entry_id)
