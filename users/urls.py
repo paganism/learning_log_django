a707730ce0301b741e25b04291e728ff6ec3f266
YuriyB <paganism@mail.ru>
2019-04-21 17:36:11 +0300

    added login/logout

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from django.contrib.auth.views import login
+from django.contrib.auth.views import LoginView
 
 from . import views
 
 urlpatterns = [
-    path('login/', login, {'template_name': 'users/login.html'}, name='login'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
