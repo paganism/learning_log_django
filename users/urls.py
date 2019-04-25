748f042aad64f5f59348027cc0c2b8adf045739b
YuriyB <paganism@mail.ru>
2019-04-25 23:45:56 +0300

    added account url

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView
+from django.conf.urls import include
+
 
 from . import views
 
@@ -7,4 +9,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register, name='register'),
+    path('account/', include('account.urls')),
 ]
