dcedc0f3f7acd61d00587a9585377ccfbe058417
YuriyB <paganism@mail.ru>
2019-04-26 21:06:27 +0300

    added some translations

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -121,6 +121,15 @@ USE_L10N = True
 
 USE_TZ = True
 
+LANGUAGES = (
+    ('ru', 'Russian'),
+    ('en', 'English'),
+)
+
+LOCALE_PATHS = (
+    'locale',
+    # os.path.join(PROJECT_DIR, 'locale'),
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
