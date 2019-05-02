e194b683450aab909084b1ab5fd66eed866c9714
YuriyB <paganism@mail.ru>
2019-05-02 15:31:15 +0300

    added drf urls

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -18,11 +18,34 @@ from django.urls import path
 from django.conf.urls import include
 from django.views.generic import RedirectView
 
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
 
 admin.site.site_header = 'Learnong Log'
+
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
-    path('', include('learning_logs.urls'))
+    path('', include('learning_logs.urls')),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
