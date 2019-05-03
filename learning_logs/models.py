f6cd5df17af1d582de8b0afe34a378f27ea8e731
YuriyB <paganism@mail.ru>
2019-05-03 20:43:07 +0300

    drf. added entries to topics methos

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -12,7 +12,7 @@ class Topic(models.Model):
 
 
 class Entry(models.Model):
-    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, related_name='entries', on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
--- rest_api/serializers.py
+++ rest_api/serializers.py
@@ -16,9 +16,15 @@ class TopicSerializer(serializers.ModelSerializer):
 
     owner = UserSerializer()
 
+    entries = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='text'
+     )
+
     class Meta:
         model = Topic
-        fields = ('id', 'text', 'date_added', 'owner')
+        fields = ('id', 'text', 'date_added', 'owner', 'entries')
 
 
 class EntrySerializer(serializers.ModelSerializer):
